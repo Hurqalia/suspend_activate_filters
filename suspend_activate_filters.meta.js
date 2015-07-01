631df3b095f4cf213299a343b7728e160739553c
Hurqalia <hurqalia22@gmail.com>
2015-07-01 17:30:51 +0200

    Change url

--- suspend_activate_filters.meta.js
+++ suspend_activate_filters.meta.js
@@ -5,9 +5,9 @@
 // @category       Info
 // @version        0.1.7.20150408.007
 // @namespace      https://github.com/Hurqalia/suspend_activate_filters
-// @updateURL      https://github.com/Hurqalia/suspend_activate_filters/suspend_activate_filters.meta.js
-// @downloadURL    https://github.com/Hurqalia/suspend_activate_filters/suspend_activate_filters.user.js
-// @installURL     https://github.com/Hurqalia/suspend_activate_filters/suspend_activate_filters.user.js
+// @updateURL      https://github.com/Hurqalia/suspend_activate_filters/raw/master/suspend_activate_filters.meta.js
+// @downloadURL    https://github.com/Hurqalia/suspend_activate_filters/raw/master/suspend_activate_filters.user.js
+// @installURL     https://github.com/Hurqalia/suspend_activate_filters/raw/master/suspend_activate_filters.user.js
 // @description    [hurqalia22-2015-04-28-007] Suspend or activate all filters.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
