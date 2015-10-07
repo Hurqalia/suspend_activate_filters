409f1f408c244fbeab8afc398f8381e778fca4f7
Hurqalia <Hurqalia@users.noreply.github.com>
2015-10-07 10:51:16 +0200

    Update suspend_activate_filters.meta.js
    Fix new Reswue layers name

--- suspend_activate_filters.meta.js
+++ suspend_activate_filters.meta.js
@@ -3,12 +3,12 @@
 // @name           IITC plugin: Suspend or activate all filters.
 // @author         hurqalia22
 // @category       Info
-// @version        0.1.7.20150408.007
+// @version        0.1.8.20151007.001
 // @namespace      https://github.com/Hurqalia/suspend_activate_filters
 // @updateURL      https://github.com/Hurqalia/suspend_activate_filters/raw/master/suspend_activate_filters.meta.js
 // @downloadURL    https://github.com/Hurqalia/suspend_activate_filters/raw/master/suspend_activate_filters.user.js
 // @installURL     https://github.com/Hurqalia/suspend_activate_filters/raw/master/suspend_activate_filters.user.js
-// @description    [hurqalia22-2015-04-28-007] Suspend or activate all filters.
+// @description    [hurqalia22-2015-10-07-001] Suspend or activate all filters.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
