b902c0a59923ada616643aaa021362cd43a77809
Hurqalia <Hurqalia@users.noreply.github.com>
2016-10-05 13:28:43 +0200

    Update suspend_activate_filters.meta.js
    Fix Niantics redirect

--- suspend_activate_filters.meta.js
+++ suspend_activate_filters.meta.js
@@ -3,15 +3,19 @@
 // @name           IITC plugin: Suspend or activate all filters.
 // @author         hurqalia22
 // @category       Info
-// @version        0.1.8.20151007.001
+// @version        0.1.9.20151007.001
 // @namespace      https://github.com/Hurqalia/suspend_activate_filters
 // @updateURL      https://github.com/Hurqalia/suspend_activate_filters/raw/master/suspend_activate_filters.meta.js
 // @downloadURL    https://github.com/Hurqalia/suspend_activate_filters/raw/master/suspend_activate_filters.user.js
 // @installURL     https://github.com/Hurqalia/suspend_activate_filters/raw/master/suspend_activate_filters.user.js
 // @description    [hurqalia22-2015-10-07-001] Suspend or activate all filters.
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
 // @grant          none
 // ==/UserScript==
